9edc737d4ba8f20ad42db1b8291b64c49779d9f2
MiguelAngelLuna <lunas123de@gmail.com>
2025-10-19 03:37:19 -0500

    nuevo

--- app/src/main/java/com/example/proyectolibro/Fragment/InfoLibroFragment.kt
+++ app/src/main/java/com/example/proyectolibro/Fragment/InfoLibroFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.proyectolibro.R
 
@@ -22,18 +24,31 @@ class InfoLibroFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Recuperar los datos enviados desde MenuFragment
+        // Recuperar los datos enviados desde el fragmento anterior
         val titulo = arguments?.getString("titulo")
         val autor = arguments?.getString("autor")
         val descripcion = arguments?.getString("descripcion")
         val precio = arguments?.getDouble("precio")
         val imagen = arguments?.getInt("imagen")
 
-        // Asignar los valores a los elementos visuales del layout
         view.findViewById<TextView>(R.id.tvTituloInfo).text = titulo
         view.findViewById<TextView>(R.id.tvAutorInfo).text = "por $autor"
         view.findViewById<TextView>(R.id.tvDescripcionInfo).text = descripcion
         view.findViewById<TextView>(R.id.tvPrecioInfo).text = "S/. $precio"
         view.findViewById<ImageView>(R.id.ivImagenInfo).setImageResource(imagen ?: 0)
+
+        // Botón Comprar
+        val btnComprar = view.findViewById<Button>(R.id.btnComprar)
+        btnComprar.setOnClickListener {
+
+            Toast.makeText(requireContext(), "Libro comprado: $titulo", Toast.LENGTH_SHORT).show()
+        }
+
+        // Botón Regresar
+        val btnRegresar = view.findViewById<Button>(R.id.btnRegresar)
+        btnRegresar.setOnClickListener {
+            // Regresa al fragmento anterior
+            requireActivity().supportFragmentManager.popBackStack()
+        }
     }
 }
